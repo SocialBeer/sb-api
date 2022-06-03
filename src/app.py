501e021620c858fa61512db8fc7a74fd73e7c0ce
n.semenchuk <n.semenchuk@itransition.com>
2022-06-03 14:39:16 +0300

    add cors

--- src/app.py
+++ src/app.py
@@ -1,13 +1,24 @@
 from fastapi.security import HTTPBasic
+from fastapi.middleware.cors import CORSMiddleware
 from fastapi import FastAPI
 
 from .users.controller import router as users_router
 from .auth.controller import router as auth_router
 
-
+origins = [
+    "http://localhost:3000",
+]
 
 app = FastAPI(
     title = 'SB-Api'
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 security = HTTPBasic()
