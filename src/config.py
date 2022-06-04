e487f5ba04e8cd4300cef353c27609c9ed938aca
n.semenchuk <n.semenchuk@itransition.com>
2022-06-05 00:06:41 +0300

    minor fix

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ class Config(BaseSettings):
     port:int = 8080
     host:str = '127.0.0.1'
     reload:bool = True
-    db_url:str = 'postgres://qykderyj:VQ0fuWJrdXxMr3JY1ycuqwSP5ERGo1mO@castor.db.elephantsql.com/qykderyj'
+    db_url:str = 'postgresql://qykderyj:VQ0fuWJrdXxMr3JY1ycuqwSP5ERGo1mO@castor.db.elephantsql.com/qykderyj'
     # TEST DB URL
     # db_url:str = 'postgresql://postgres:06082003@localhost:5432/Sb-sb-test'
     secret_access_token_key: str = 'd86c828583c5c6160e8acfee88ba1590'
